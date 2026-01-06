9cba728f483886ee49cd88a6dbabd71ddd260873
Edilson Oliveira <edilsonoliveira@me.com>
2026-01-06 12:03:47 -0300

    Fix Railway healthcheck and add health endpoint

--- core/urls.py
+++ core/urls.py
@@ -1,18 +1,18 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import RedirectView
+from django.http import HttpResponse
 
-
+def health_check(request):
+    return HttpResponse("OK")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('health/', health_check, name='health'),  # Nova rota para healthcheck
     path('accounts/', include('accounts.urls', namespace='accounts')),
     path('', RedirectView.as_view(pattern_name='accounts:login'), name='home'),
 
     path('products/', include('products.urls', namespace='products')),
-
     path('orders/', include('orders.urls', namespace='orders')),
-
     path('checkouts/', include('checkouts.urls', namespace='checkouts')),
-
 ]
