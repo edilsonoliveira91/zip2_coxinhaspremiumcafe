09b420e8c9145eafa35e208c96d865bb70695c9b
Edilson Oliveira <edilsonoliveira@me.com>
2026-05-28 09:48:05 -0300

    Adjust: storage R3 Cloudflare

--- core/settings.py
+++ core/settings.py
@@ -170,6 +170,23 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
+# ============================================================
+# CLOUDFLARE R2 - Storage de arquivos de mídia (produção)
+# ============================================================
+USE_R2_STORAGE = config('USE_R2_STORAGE', default=False, cast=bool)
+
+if USE_R2_STORAGE:
+    DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+    AWS_ACCESS_KEY_ID = config('R2_ACCESS_KEY_ID')
+    AWS_SECRET_ACCESS_KEY = config('R2_SECRET_ACCESS_KEY')
+    AWS_STORAGE_BUCKET_NAME = config('R2_BUCKET_NAME')
+    AWS_S3_ENDPOINT_URL = config('R2_ENDPOINT_URL')
+    AWS_S3_CUSTOM_DOMAIN = config('R2_PUBLIC_URL')
+    AWS_DEFAULT_ACL = None
+    AWS_S3_FILE_OVERWRITE = False
+    AWS_QUERYSTRING_AUTH = False
+    MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/'
+
 IMAGE_UPLOAD_MAX_MB = config('IMAGE_UPLOAD_MAX_MB', default=8, cast=int)
 
 # Configurações do Tailwind
